63e9a0b64ee270dca07da3daeaa2c94a9e8da04c
Erick Arostegui Cunza <scorpius86@gmail.com>
2021-01-29 19:39:22 -0500

    Session 02

---
+++ 01/HelloWolrdLinux/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWolrdLinux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Linux!");
+        }
+    }
+}
---
+++ 01/HelloWorldWindows/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldWindows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Windows!");
+        }
+    }
+}
